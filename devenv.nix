ed16e77ce85cf285ae2daa52dbf219ac03d6104b
Menezess42 <arielmenezess@hotmail.com>
2026-07-14 09:51:06 -0300

    updating devenv to have apyori python lib

--- devenv.nix
+++ devenv.nix
@@ -1,16 +1,8 @@
 { pkgs, lib, config, inputs, ... }:
-
 {
     packages = [
         pkgs.pyright
-        # pkgs.qt5.qtbase.bin
-        # pkgs.qt5.qtwayland.bin
     ];
-
-    # env = {
-    #     QT_QPA_PLATFORM_PLUGIN_PATH = "${pkgs.qt5.qtbase.bin}/lib/qt-5.15/plugins";
-    # };
-    #
     languages.python = {
         enable = true;
         package = pkgs.python313.withPackages (p: with p; [
@@ -19,14 +11,12 @@
             scipy
             pandas
             matplotlib
-            # scikit-image
             pytest
             ipykernel
             ipython
             kaggle
             plotly
             nbformat
-            # (p.opencv4.override { enableGtk3 = true; enableFfmpeg = true; })
             pyqt5
             scikit-learn
             pip
@@ -42,9 +32,54 @@
             debugpy
         ]);
         venv.enable = true;
+        venv.requirements = ''
+            apyori==1.1.2
+        '';
     };
-
     enterShell = ''
       echo "$(python --version) — venv ativo"
     '';
 }
+# { pkgs, lib, config, inputs, ... }:
+#
+# {
+#     packages = [
+#         pkgs.pyright
+#     ];
+#
+#     languages.python = {
+#         enable = true;
+#         package = pkgs.python313.withPackages (p: with p; [
+#             kagglehub
+#             numpy
+#             scipy
+#             pandas
+#             matplotlib
+#             #apyori
+#             pytest
+#             ipykernel
+#             ipython
+#             kaggle
+#             plotly
+#             nbformat
+#             pyqt5
+#             scikit-learn
+#             pip
+#             jedi
+#             jedi-language-server
+#             black
+#             flake8
+#             sentinel
+#             python-lsp-server
+#             virtualenv
+#             pyflakes
+#             isort
+#             debugpy
+#         ]);
+#         venv.enable = true;
+#     };
+#
+#     enterShell = ''
+#       echo "$(python --version) — venv ativo"
+#     '';
+# }
